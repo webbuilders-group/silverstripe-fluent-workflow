e2e9dded70a6764c1aae016614274b4bd3f133ac
Robert LeCreux <robertlecreux@hotmail.com>
2019-07-04 15:49:30 -0300

    removed docblocks

--- src/DataObjects/FluentWorkflowInstance.php
+++ src/DataObjects/FluentWorkflowInstance.php
@@ -11,38 +11,11 @@ use Symbiote\AdvancedWorkflow\DataObjects\WorkflowInstance;
 use Symbiote\AdvancedWorkflow\DataObjects\WorkflowDefinition;
 use Symbiote\AdvancedWorkflow\DataObjects\WorkflowActionInstance;
 
-/**
- * A WorkflowInstance is created whenever a user 'starts' a workflow.
- *
- * This 'start' is triggered automatically when the user clicks the relevant
- * button (eg 'apply for approval'). This creates a standalone object
- * that maintains the state of the workflow process.
- *
- * @method WorkflowDefinition Definition()
- * @method WorkflowActionInstance CurrentAction()
- * @method Member Initiator()
- *
- * @author  marcus@symbiote.com.au
- * @license BSD License (http://silverstripe.org/bsd-license/)
- * @package advancedworkflow
- */
 class FluentWorkflowInstance extends WorkflowInstance
 {
 
     private static $table_name = "FluentWorkflowInstance";
 
-    /**
-     * Get the target-object that this WorkflowInstance "points" to.
-     *
-     * Workflows are not restricted to being active on SiteTree objects,
-     * so we need to account for being attached to anything.
-     *
-     * Sets Versioned::set_reading_mode() to allow fetching of Draft _and_ Published
-     * content.
-     *
-     * @param boolean $getLive
-     * @return null|DataObject
-     */
     public function getTarget($getLive = false)
     {
         return FluentState::singleton()->withState(function ($state) use ($getLive) {
