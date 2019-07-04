286724ba8a82cda022dfe2374cdf0438299360a0
Robert LeCreux <robertlecreux@hotmail.com>
2019-07-04 13:35:47 -0300

    Overwrote getWorkflow method

--- src/services/FluentWorkflowService.php
+++ src/services/FluentWorkflowService.php
@@ -2,8 +2,15 @@
 
 namespace WebbuildersGroup\FluentWorkflow\Services;
 
+use SilverStripe\Core\Convert;
+use SilverStripe\Core\ClassInfo;
 use SilverStripe\ORM\DataObject;
+use TractorCow\Fluent\Model\Locale;
+use TractorCow\Fluent\State\FluentState;
 use Symbiote\AdvancedWorkflow\Services\WorkflowService;
+use Symbiote\AdvancedWorkflow\DataObjects\WorkflowInstance;
+use Symbiote\AdvancedWorkflow\Extensions\WorkflowApplicable;
+use Symbiote\AdvancedWorkflow\Extensions\FileWorkflowApplicable;
 use Symbiote\AdvancedWorkflow\Services\ExistingWorkflowException;
 use WebbuildersGroup\FluentWorkflow\DataObjects\FluentWorkflowInstance;
 
@@ -35,6 +42,47 @@ class FluentWorkflowService extends WorkflowService
             $instance = FluentWorkflowInstance::create();
             $instance->beginWorkflow($definition, $object);
             $instance->execute();
+        }
+    }
+
+    /**
+     * Gets the workflow for the given item
+     *
+     * The item can be
+     *
+     * a data object in which case the ActiveWorkflow will be returned,
+     * an action, in which case the Workflow will be returned
+     * an integer, in which case the workflow with that ID will be returned
+     *
+     * @param mixed $item
+     * @param bool $includeComplete
+     * @return WorkflowInstance|null
+     */
+    public function getWorkflowFor($item, $includeComplete = false)
+    {
+        $locale = Locale::getCurrentLocale()->Locale;
+
+        $id = $item;
+
+        if ($item instanceof WorkflowAction) {
+            $id = $item->WorkflowID;
+            return DataObject::get_by_id(WorkflowInstance::class, $id);
+        } elseif (
+            is_object($item) && ($item->hasExtension(WorkflowApplicable::class)
+                || $item->hasExtension(FileWorkflowApplicable::class))
+        ) {
+            $filter = sprintf(
+                '"TargetClass" = \'%s\' AND "TargetID" = %d AND "TargetLocale" = \'%s\' ',
+                Convert::raw2sql(ClassInfo::baseDataClass($item)),
+                $item->ID,
+                $locale
+            );
+            $complete = $includeComplete ? 'OR "WorkflowStatus" = \'Complete\' ' : '';
+            $do = FluentWorkflowInstance::get()->where(
+                $filter . ' AND ("WorkflowStatus" = \'Active\' OR "WorkflowStatus"=\'Paused\' ' . $complete . ')'
+            )->first();
+
+            return $do;
         }
     }
 }
