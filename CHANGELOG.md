07e681f741157d911a9e0c1060112a2d6baaba60
UndefinedOffset <echipman@webbuildersgroup.com>
2019-08-13 13:00:48 -0300

    Added fluent icon to the publishing schedule fields (fixes #3)

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [1.0.1](https://github.com/webbuilders-group/silverstripe-new-relic/tree/1.0.1) (2019-08-13)
+[Full Changelog](https://github.com/webbuilders-group/silverstripe-new-relic/compare/1.0.0...1.0.1)
+
+## [1.0.0](https://github.com/webbuilders-group/silverstripe-new-relic/tree/1.0.0) (2019-07-10)
+* First Stable Release
--- src/Extensions/FluentWorkflowEmbargoExpiryExtension.php
+++ src/Extensions/FluentWorkflowEmbargoExpiryExtension.php
@@ -1,6 +1,8 @@
 <?php
 namespace WebbuildersGroup\FluentWorkflow\Extensions;
 
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\FieldType\DBField;
 use Symbiote\AdvancedWorkflow\Extensions\WorkflowEmbargoExpiryExtension;
 
 class FluentWorkflowEmbargoExpiryExtension extends WorkflowEmbargoExpiryExtension
@@ -15,6 +17,39 @@ class FluentWorkflowEmbargoExpiryExtension extends WorkflowEmbargoExpiryExtensio
         'UnPublishJobID',
     ];
     
+    /**
+     * Updates the fields used in the cms
+     * @param FieldList $fields Fields to be extended
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        parent::updateCMSFields($fields);
+        
+        $translatedTooltipTitle = _t(__CLASS__ . ".FLUENT_ICON_TOOLTIP", 'Translatable field');
+        $tooltip = DBField::create_field('HTMLFragment', "<span class='font-icon-translatable' title='$translatedTooltipTitle'></span>");
+        
+        
+        //Update the publish date field to have the icon
+        $field = $fields->dataFieldByName('PublishOnDate');
+        if (!$field->hasClass('fluent__localised-field')) {
+            $field
+                ->setTitle(DBField::create_field('HTMLFragment', $tooltip . $field->Title()))
+                ->addExtraClass('fluent__localised-field');
+        }
+        
+        
+        //Update the unpublish date field to have the icon
+        $field = $fields->dataFieldByName('UnPublishOnDate');
+        if (!$field->hasClass('fluent__localised-field')) {
+            $field
+                ->setTitle(DBField::create_field('HTMLFragment', $tooltip . $field->Title()))
+                ->addExtraClass('fluent__localised-field');
+        }
+    }
+    
+    /**
+     * Clears the workflow fields when localizing
+     */
     public function onBeforeWrite()
     {
         parent::onBeforeWrite();
